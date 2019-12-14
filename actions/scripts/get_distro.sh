c0d65bccddf5b2c109d3593e00e3795b0fbbcf44
gkzz <gakuji.tamaki@gmail.com>
2019-12-14 17:53:53 +0900

    hotfix

--- actions/scripts/get_distro.sh
+++ actions/scripts/get_distro.sh
@@ -4,7 +4,7 @@ set -e
 
 distro=$1
 
-content=$(cat /etc/os-release | grep -E "ID=$distro" | awk '{print $6}' | grep -oP "$distro")
+content=$(cat /etc/os-release | grep -E "ID=$distro" | awk '{print $1}' | grep -oP "$distro")
 
 output=$(echo ${content:="unknown"})
 echo "output: $output"
