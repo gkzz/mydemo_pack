aeb3d80d3eeb5d12556b4e9f3ac449e4cff7b672
gkzz <gakuji.tamaki@gmail.com>
2019-12-15 15:47:14 +0900

    hotfix

--- actions/scripts/git_status.py
+++ actions/scripts/git_status.py
@@ -9,46 +9,46 @@ from common_mydemo import Common
 class GitStatusAction(Action):
     """Action class for st2"""
 
-    def __init__(self, config, working_dir, branch, expected):
+    def __init__(self, config):
         self.output = []
         self.result = {}
         self.config = config
-        self.command = "cd {dir} && sudo git status".format(dir=working_dir)
-        self.branch = branch
-        self.expected = expected
-        self.redict = {
-            'up_to_date': re.compile(
-                #   1       2          3      4              5        6
-                "r(Your)\s+(branch)\s+(is)\s+(up-to-date)\s+(with)\s+('origin/" + re.escape(self.branch) + "')"
-            ),
-            'not_up_to_date': re.compile(
-                "r(Your)\s+(branch)\s+(is)\s+(behind)\s+('origin/" + re.escape(self.branch) + "')"
-            ),
-        }
         for r in [
-            "command", "branch", "expected", "bool", "stdout", "stderr"
+            "command", "branch", "expected", "bool", "stdout", "stderr"        
         ]:
             self.result[r] = None 
 
         self.common = Common()
+        return
 
+    def set_command(self, dir):
+      return "cd {dir} && sudo git status".format(dir=dir)
 
-    
-    def _set_regex(self):
+    def _set_regex(self, branch, expected):
+        redict = {
+            'up_to_date': re.compile(
+                #   1       2          3      4              5        6
+                "r(Your)\s+(branch)\s+(is)\s+(up-to-date)\s+(with)\s+('origin/" + re.escape(branch) + "')"
+            ),
+            'not_up_to_date': re.compile(
+                "r(Your)\s+(branch)\s+(is)\s+(behind)\s+('origin/" + re.escape(branch) + "')"
+            ),
+        }
+
         ptn = None
-        if self.expected == "up_to_date":
-            ptn = self.redict['up_to_date']
-        elif self.expected == "not_up_to_date":
-            ptn = self.redict['not_up_to_date']
+        if expected == "up_to_date":
+            ptn = redict['up_to_date']
+        elif expected == "not_up_to_date":
+            ptn = redict['not_up_to_date']
         else:
             pass
 
         return ptn
     
 
-    def check_stdout(self, stdout):
+    def check_stdout(self, branch, expected, stdout):
         success = False
-        ptn = self._set_regex()
+        ptn = self._set_regex(branch, expected)
         for line in stdout:
             if not success and ptn.search(line):
                 success = True
@@ -63,17 +63,17 @@ class GitStatusAction(Action):
         return success, stdout
     
 
-    def write_result(self, bool, stdout, stderr):
-        result.update({
-            "command": self.command,
-            "branch": self.branch,
-            "expected": self.expected,
-            "bool": self.bool,
+    def write_result(self, command, branch, expected, bool, stdout, stderr):
+        self.result.update({
+            "command": command,
+            "branch": branch,
+            "expected": expected,
+            "bool": bool,
             "stdout": stdout,
             "stderr": stderr,
         })
 
-        return result
+        return self.result
     
 
     def run(self, working_dir, branch, expected):
@@ -84,25 +84,16 @@ class GitStatusAction(Action):
         stderr = []
 
         try:
-            self.bool, stdout, stderr = self.common.execute_command(self.command)
-            bool, stdout = self.check_stdout(stdout)
-            self.output = self.write_result(bool, stdout, stderr)
+            command = self.set_command(working_dir)
+            bool, stdout, stderr = self.common.execute_command(command)
+            bool, stdout = self.check_stdout(branch, expected, stdout)
+            self.result = self.write_result(command, branch, expected, bool, stdout, stderr)
         except:
             pass
 
         finally:
-            self.result = self.write_result(bool, stdout, stderr)
+            self.result = self.write_result(command, branch, expected, bool, stdout, stderr)
         
         self.output.append(self.result)
 
         return self.output 
-
-
-
-
-
-
-
-
-
-
